23dd28f55f4f1e411c3ff58b88771e335a28e9dc
maulana.anas <maulana.anas@JKT0830.csf.co.id>
2023-05-03 13:29:36 +0700

    First Commit

---
+++ Scripts/ORD-003_Verify user failed order a product if product was empty/Script1683079290368.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('D:\\01 Tiket on Progress\\Surplus_base.apk', false)
+
+Mobile.tap(findTestObject('Order to delivery/Search Bar Home - Field'), 0)
+
+Mobile.setText(findTestObject('Order to delivery/Search Bar History - Field'), 'Spaghetti', 0)
+
+Mobile.tap(findTestObject('Order to delivery/Searh Icon - Button'), 0)
+
+Mobile.tap(findTestObject('Order to delivery/First Shop - List Group'), 0)
+
+Mobile.tap(findTestObject('Order to delivery/Pesan Item Kosong - Button'), 0)
+
+Mobile.verifyElementVisible(findTestObject('Order to delivery/Menu Tidak Tersedia - Pop Up'), 0)
+
+Mobile.tap(findTestObject('Order to delivery/Ok - Button Menu Tidak Tersedia'), 0)
+
---
+++ Test Suites/Regression.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e6144a1e-14f0-4ca0-8489-bcc7fdef6dbb</testSuiteGuid>
+   <testCaseLink>
+      <guid>aad900bb-5839-409c-92a5-cbb0323c743f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ORD-001_Verify user success order a product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b5552781-06db-43bc-a859-0744cbfb8220</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ORD-002_Verify user success add more than 1 item into shopping cart</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5512e052-19b2-4374-942c-c68af6231b89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ORD-003_Verify user failed order a product if product was empty</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
